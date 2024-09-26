37e17495bad34a1b851be8ae48e4e135a56e393a
sameom1048 <samvision99@gmail.com>
2024-09-26 19:36:19 +0900

    update commit

---
+++ src/main/java/com/project/hairshop/HairshopApplication.java
@@ -0,0 +1,13 @@
+package com.project.hairshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HairshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/hairshop/HairshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hairshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
